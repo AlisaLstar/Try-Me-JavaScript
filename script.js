52bfd8c75e65c871d3ee3f55c446df98e9f62549
Alisa L <lungsuaya2@gmail.com>
2020-09-06 12:29:26 -0400

    Added JS and outline JS framework

--- script.js
+++ script.js
@@ -1,42 +1,100 @@
 // assign vars
 // classese
-"jumbotron" 
-
-
-
+"jumbotron";
 
 // IDs
-"instrPage"
-"startBtn"
+"instrPage";
+"startBtn";
 
-"codeQuests"
-"ansChoices"
+"codeQuests";
+"ansChoices";
 
-"divAlertAns"
-"totalScores"
-"scoreResult"
-"userInitial"
-"inputInitial" 
-"sumitBtn"
-"wellDone"
+"divAlertAns";
+"totalScores";
+"scoreResult";
+"userInitial";
+"inputInitial";
+"sumitBtn";
+"wellDone";
+var questions = [
+  {
+    questions: "What is not included in flowshart key?",
+    answer: [
+      {
+        text: "Note",
+        correct: true,
+        text: "Event",
+        correct: false,
+        text: "Decision",
+        correct: false,
+        text: "Input",
+        correct: false,
+      },
+    ],
+  },
 
-var startBtn = document.getElementById("startBtn");
+  {
+    questions: "Which one is not representing the use of variables?",
+    answer: [
+      { text: "Numbers", result: false },
+      { text: "Reserch", result: true },
+      { text: "Data", resultt: false },
+      { text: "Names", result: false },
+    ],
+  },
+
+  {
+    questions: "When asking a function to perform its task, it is known as?",
+    answer: [
+      { text: "Calling the function", result: true },
+      { text: "Join the function", result: false },
+      { text: "Rename the function", result: false },
+      { text: "Perform the function", result: true },
+    ],
+  },
+
+  {
+    questions: "What is not part of three groups of built-in objects?",
+    answer: [
+      { text: "Global JavaScipt Objects", result: false },
+      { text: "Document Object Model", result: false },
+      { text: "Browser Object Model", result: false },
+      { text: "Double Three Objects", result: true },
+    ],
+  },
+
+  {
+    questions:
+      "Which is not an object represents basic data type of Global JavaScript Objects?",
+    answer: [
+      { text: "String", result: false },
+      { text: "Number", result: false },
+      { text: "Document", result: true },
+      { text: "Boolean", result: false },
+    ],
+  },
+];
+var startButtonEl = document.getElementById("startButtonEl");
+
 var currentQuestion;
-var availableQuestions = [...quests];
+var availableQuestions = [...questions];
 var questionsIndex = 0;
 var questionCounter = 0;
 var acceptingAnswer = true;
-var correctanswer= 0;
+var correctanswer = 0;
 var time = 180;
 var scores = 0;
-var start = document.querySelector("#startHere")
+var start = document.querySelector("#codeQuests");
 var toDIsplayAnswer = document.querySelectorAll("#ansChoices");
-var arrayIndex= true;
+var arrayIndex = true;
 var toHide = true;
 
-var timer= addEventListener("#click",)
+var timer = addEventListener("#click", startBtn);
+var startQuizEl = document.getElementById("instrPage");
+var codeQuestionsEl = document.getElementById("codeQuests");
+var resultUserEl = document.getElementById("divAlertAns");
 
-var answerChoiceBtn = document.querySelectorById("btn-questOne"), document.querySelectorById("btn-questTwo"), document.querySelectorById("btn-questThree"), document.querySelectorById("btn-questFour")
+
 
 // var quests = [ {
 //     question : "How are you doing?",
@@ -51,104 +109,121 @@ var answerChoiceBtn = document.querySelectorById("btn-questOne"), document.query
 //     correctAnswer: 1}
 // ]
 
-var questions = [
-    { questions: "What is not included in flowshart key?",
-      answer: [{text: "Note", correct: true,
-                text: "Event", correct: false,
-                text: "Decision", correct: false,
-                text: "Input", correct: false}]
-}
-
-{ questions: "Which one is not representing the use of variables?",
-answer: [{text: "Numbers", result: false},
-          {text: "Reserch", result: true},
-          {text: "Data", resultt: false},
-          {text: "Names",result: false}]
-}
-
-{ questions: "When asking a function to perform its task, it is known as?",
-answer: [{ text: "Calling the function", result: true},
-          {text: "Join the function", result: false},
-         {text: "Rename the function", result: false},
-          {text: "Perform the function", result: true}]
-}
-
-
-{ questions: "What is not part of three groups of built-in objects?",
-answer: [{text: "Global JavaScipt Objects", result: false},
-         {text: "Document Object Model", result: false},
-          {text: "Browser Object Model", result: false},
-          {text: "Double Three Objects", result: true}]
-}
-
-
-{ questions: "Which is not an object represents basic data type of Global JavaScript Objects?",
-answer: [{text: "String", result: false},
-          {text: "Number", result: false},
-          {text: "Document", result: true},
-          {text: "Boolean", result: false}]
-}
-]
-
-
 //writing functions
-function startTimer () {
-    hideStarter();
-    getQuestion();
-    var secondElapsed=0;
-    var totalSeconds= 180;
-    var timerInterval = setInterval(function() {
-        secondElapsed++;
-        if (secondElapsed >= totalSeconds) {
-            clearInterval (timerInterval);
-            return;
-            //end game();
-        } else {
-            var secondsLeft =(totalSeconds - secondElapsed);
-            // minutesDIsplay.textContent = Math.floor(secondsLeft /60);
-            // secondsDisplay.textContent =(secondsLeft %60)
-            secondsDisplay.textContent =secondLeft;
-        }
-    }, 10000);
+
+function startBtn() {
+  console.log("startbutton");
+  startQuiz();
+  getQuestion();
+  startTimer();
 }
 
-availableQuestions.splice(random,1);
-acceptingAnswers =true;
-
-function startTimer()
-
-
-function hideStarter() {
-    if (toHide) {
-
+function startTimer() {
+  hideStarter();
+  getQuestion();
+  var secondElapsed = 0;
+  var totalSeconds = 180;
+  var timerInterval = setInterval(function () {
+    secondElapsed++;
+    if (secondElapsed >= totalSeconds) {
+      clearInterval(timerInterval);
+      return;
+      //end game();
+    } else {
+      var secondsLeft = totalSeconds - secondElapsed;
+      minutesDIsplay.textContent = Math.floor(secondsLeft /60);
+      secondsDisplay.textContent =(secondsLeft %60)
+      secondsDisplay.textContent = secondLeft;
     }
+  }, 1000);
 }
 
+// availableQuestions.splice(random,1);
+// acceptingAnswers =true;
 
+function startQuiz() {
+  startQuizEl.style.display = "none";
+}
 
-function getQuestion () {
-    questionsCounter++;
-    var random = Math.floor(Math.random() * availableQuestions.length);
-    currentQuestion = availableQuestions[random];
-    showQuestions.textContent = currentQuestion.question;
+// if (toHide) { document.getElementById("instrPage", class="display: none");
+// document.getElementById("codeQuests", class="display");
+// if(toStartQuiz) {
+//     toStartQuiz = false;
+//     startSec.setAttribute("id", "noneDisplay")
+//     questionsSec.removeAttribute("id", "noneDisplay")
+// }
 
-    for (var i=0; i < toDIsplayAnswer.length; i++) {
-        var buttonIndexing = toDIsplayAnswer[i].dataset['id'];
-        toDIsplayAnswer[buttonIndexing].textContent =currentQuestion.answers[buttonIndexing];
+function getQuestion() {
+    if (availableQuestions.length ==0){
+        localStorage.setItem("latestScore", score);
+        return window.location.assign("endGame.html")
     }
-    availableQuestions.splice(random,1);
-acceptingAnswers =true;
+  codeQuestionsEl.style.display = "block";
+  var questionChoicesEl = document.getElementById("questionChoices");
+  
+//   questionsCounter++;
+  var random = Math.floor(Math.random() * questions.length);
+  var currentQuestion = questions[random];
+  questionChoicesEl.textContent = currentQuestion.question;
+
+  for (var i = 0; i < currentQuestion.answer.length; i++) {
+    var buttonEl = document.getElementById(i);
+   buttonEl.textContent=currentQuestion.answer[i].text;
+  }
+   questions.splice(random, 1);
+//   acceptingAnswers = true;
 }
 
-var availableQuestions= [...questions];
-var currentQuestion;
-var showQuestions= document.getElementById("");
+// var availableQuestions= [...questions];
 
+var showQuestions = document.getElementById("");
 
-//add event listener 
-startBtn.addEventListener("click", hideStarter);
-startBtn.addEventListener("click", startBtn);
-
-
+function incrementScore(number) {
+  scoreCounText.textContent = score;
+}
+//add event listener
+resultUserEl.style.display = "none";
+codeQuestionsEl.style.display = "none";
+startButtonEl.addEventListener("click", startBtn);
 
 //anything that said data- something, computer has it in the dom...data----dataset id: 0
+
+
+
+
+
+toDisplayAnswer.forEach(toDisplayAnswer=>){
+toDIsplayAnswer.addEventListener("click", e =>{
+
+    e.preventDefault();
+    e.stopPropagation();
+    if (!acceptingAnswer) {return};
+    acceptingAnswer= false;
+    var selectedChoice= e.target;
+    var crossAnswer = selectedChoice.dataset['id'];
+    console.log (crossAnswer == currentQuestion.answer);
+
+    if (crossAnswer== currentQuestion.answer){
+        incrementScore(scoreBonus);
+        score++;
+
+        outcomeDiv.removeAttribute("class, hide");
+        outcome.textContent = "correct";
+        setTimeout (()=>{
+            outcomeDiv.setAttribute("class", "hide");
+            getQuestion();
+        }, 1000);
+
+    }
+    else {
+        outcomeDiv.removeAttribute("class, hide");
+        outcome.textContent = "incorrect";
+        setTimeout (()=>{
+            outcomeDiv.setAttribute("class", "hide");
+            getQuestion();
+        }, 300);
+    }
+})
+}
+
+endGame 
